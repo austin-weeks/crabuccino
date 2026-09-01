4ae04e1ce7c968c181c9aa40b2b0dd11547fb911
Austin Weeks <168379071+austin-weeks@users.noreply.github.com>
2026-08-31 22:06:47 -0400

    add release workflow

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  attw: {
+    profile: "esm-only",
+  },
+  banner: {
+    dts: `// crabuccino
+//
+// Distributed under the MPL-2.0 License
+// ${new Date().toISOString()}
+`,
+    js: `// crabuccino
+//
+// Distributed under the MPL-2.0 License
+// ${new Date().toISOString()}
+`,
+  },
   minify: true,
   dts: {
     tsgo: true,
   },
   exports: true,
-  // ...config options
 });
